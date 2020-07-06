753b505599896cd8cd56526693ee2bd25b39e1f1
Alekha32 <67892782+Alekha32@users.noreply.github.com>
2020-07-06 12:48:48 +0530

    Add files via upload

---
+++ ConditionalBranches.java
@@ -0,0 +1,45 @@
+package controlStructures;
+
+import java.util.Scanner;
+
+public class ConditionalBranches {
+
+	
+	public static void main(String[] args) {
+		
+		
+		//  int age = 13;// local variable 
+			/*
+			 * if(age > 18) { System.out.println("Eligible for vote " +age); } else if(age >
+			 * 13 && age < 18) { System.out.println("teen age " + age); } else {
+			 * System.out.println("Child " + age); }
+			 */
+			/*
+			 * int count = 3; switch(count) { case 0: System.out.println("Print zero");
+			 * break; case 1: System.out.println("Print one"); break; case 2:
+			 * System.out.println("print two"); break; case 3:
+			 * System.out.println("Print three"); break; default:
+			 * System.out.println("Print Null");
+			 * 
+			 * }
+			 */
+			/*
+			 * Scanner sc = new Scanner(System.in);
+			 * 
+			 * int [] a = new int[10]; for(int i=0;i<a.length;i++) { a[i] = sc.nextInt();
+			 * System.out.println(a[i]); }
+			 */
+			/*
+			 * int i =1;
+			 *  while(i<10) {
+			 *  if(i==5) {
+			 *  continue;
+			 *  }
+			 *  System.out.println(i); i++; }
+			 */
+			/*
+			 * int j =1; do { System.out.println(j); j++; } while(j<=10);
+			 */
+		 
+	}
+}
